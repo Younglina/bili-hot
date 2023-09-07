45a1e3d753727caf29dcb9cc86407ec6559783a9
younglina <younglina0409@gmail.com>
2023-09-07 14:49:39 +0800

    定时提交

--- bili-hot-end/index.js
+++ bili-hot-end/index.js
@@ -2,6 +2,7 @@ const koa = require('koa')
 const koaRouter = require('koa-router')
 const koaCors = require('koa-cors')
 const cron = require('node-cron');
+const { exec } = require('child_process');
 const { getPopularlist, addPopularlistByDate  } = require('./usePopular.js')
 
 const app = new koa()
@@ -27,8 +28,23 @@ app.use(router.allowedMethods({throw: true}))
 cron.schedule('30 23 * * *', () => {
   addPopularlistByDate()
   console.log('执行任务');
+  commitToGit();
 });
+
+
+// 定义提交函数
+function commitToGit() {
+  exec('git add . && git commit -m "定时提交" && git push', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`提交到Git仓库时出错：${error.message}`);
+      return;
+    }
+    console.log('提交成功');
+  });
+}
+
 
 app.listen(process.env.PORT || 3000, ()=>{
   console.log('server listen 3000')
+  commitToGit()
 })
